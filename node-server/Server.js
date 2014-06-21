245dbf3f6c4610be35b7358512831c8a8102c110
ayumi.goto <ayumi.goto@jp.oro.com>
2014-06-21 18:11:02 +0900

    サーバーアドレス修正

--- node-server/Server.js
+++ node-server/Server.js
@@ -5,5 +5,5 @@ var webRadiconServer = WebRadiconServer.createServer();
 webRadiconServer.listen({ 
 	controllerPort 	: 8080,
 	arduinoPort 	: 3000,
-	ip				: "49.212.49.143"
+	ip				: "128.199.167.229"
 });
